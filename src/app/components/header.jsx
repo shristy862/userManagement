b25245646ec902c8ca6040cb4fdccc9e33a68995
Shristy Verma <shristyv301@gmail.com>
2024-10-01 19:51:41 +0530

    Your descriptive commit message here

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -2,11 +2,11 @@
 import React from 'react';
 import './style.css';
 
-const Header = ({ userInfo, dashboardMessage }) => {
+const Header = ({ userInfo }) => {
   return (
     <header className="custom-header">
       <div className="header-content">
-        <h1 className="logo">{dashboardMessage && <p>{dashboardMessage}</p>}</h1>
+        
         <nav className="navigation">
           {userInfo && <p>Welcome, {userInfo.role}!</p>}
            
